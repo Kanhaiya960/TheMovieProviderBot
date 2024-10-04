2a3616d1c35aad69686669bb527cbe4f52b8d2d2
♥⁠╣Sivam_uv╠⁠♥ <123348827+Botprim@users.noreply.github.com>
2024-10-04 15:25:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -127,7 +127,7 @@ else:
     URL = "https://tomandjerstreamsivam-4c7e90fa9181.herokuapp.com/".format(FQDN)
 
 # add premium logs channel id
-PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', ''))
+PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002036619716'))
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
