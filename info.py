cb025a59cba0e63160b397c9c798bde11406a098
Kanhaiya960 <168913838+Kanhaiya960@users.noreply.github.com>
2024-12-20 07:43:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,8 +55,8 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Verify
-VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( False )
-HOWTOVERIFY = environ.get('HOWTOVERIFY', url='https://t.me/Movies_4_Download') # How to open tutorial link for verification
+VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( False )HOWTOVERIFY = environ.get('HOWTOVERIFY', 'https://t.me/Movies_4_Download')
+HOWTOVERIFY = environ.get('HOWTOVERIFY', 'https://t.me/Movies_4_Download') # How to open tutorial link for verification
 
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'instantearn.in')
